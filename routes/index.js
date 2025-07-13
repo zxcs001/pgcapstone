f201aa2ce2710f8e26e860222d69c6e36c6f5dee
Yuchen Iochi <zxcs001@gmail.com>
2025-07-13 10:16:12 -0400

    update auth

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,45 @@
+var globalConstant = require('../variables')
 var express = require('express');
 const axios = require('axios').default;
 var router = express.Router();
 
-var token = ""
-var user = {}
+const auth = (req, res, next) => {
+   
+    if (!req.cookies) {
+        res.render('login', { title: '' });
+        return
+    }
+
+    // We can obtain the session token from the requests cookies, which come with every request
+    const sessionToken = req.cookies['session_token']
+    if (!sessionToken) {
+        // If the cookie is not set, return an unauthorized status
+        res.redirect(307, "/login");
+        return
+    }
+
+    // We then get the session of the user from our session map
+    // that we set in the signinHandler
+    userSession = sessions[sessionToken]
+    console.log('here is cookies ', userSession);
+    if (!userSession) {
+        // If the session token is not present in session map, return an unauthorized error
+        res.redirect(307, "/login");
+        return
+    }
+    // if the session has expired, return an unauthorized error, and delete the 
+    // session from our map
+    if (userSession.isExpired()) {
+        delete sessions[sessionToken]
+        res.redirect(307, "/login");
+        return
+    }
+
+  next()
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', auth, function(req, res, next) {
   // business logic here
   // for example fetch data from our backend
   // prepare data for our html website
@@ -17,6 +51,13 @@ router.get('/checkout', function(req, res, next) {
   // for example fetch data from our backend
   // prepare data for our html website
   res.render('checkout', { title: 'This is example form Yuchen' });
+});
+
+router.get('/cart', function(req, res, next) {
+  // business logic here
+  // for example fetch data from our backend
+  // prepare data for our html website
+  res.render('cart', { title: 'This is example form Yuchen' });
 });
 
 router.get('/contact', function(req, res, next) {
@@ -31,9 +72,14 @@ router.get('/users', function(req, res, next) {
   res.render('signup', { title: 'This is example form Yuchen' });
 });
 
-router.get('/login', async function(req, res, next) {
+router.get('/shop', function(req, res, next) {
+  // user profile
+  res.render('shop', { title: 'This is example form Yuchen' });
+});
+
+router.get('/login', function(req, res, next) {
   // login-page
-  console.log('this is token',token)
+  // console.log('this is token',token)
   res.render('login', { title: '' });
 });
 
@@ -42,28 +88,69 @@ router.get('/history', function(req, res, next) {
   res.render('orderhistory', { title: 'This is example form Yuchen' });
 });
 
-router.get('/login-api', async function(req, res, next) {
-  // backend call for login
+router.post('/login', async function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  //console.log('req body',req.body)
+  //test data
   const data = {
     "email": "annikat.harmsen1000@gmail.com",
     "password": "secure123"
   }
+
   try {
-      response = await axios.post('http://localhost:8000/api/login', data)
-      console.log(response.data)
-      if (response.data = true) {
-        token = response.data.data.token;
-        user = response.data.data.user;
-        console.log(response.data.data)
-        res.render('index', { title: '' });
+      response = await axios.post(globalConstant.backendApi+'/login', data)
+      console.log('response is here' ,response.data.data)
+      if (response.data.data) { 
+        let token = response.data.data.token;
+        let user = response.data.data.user;
+        //console.log(response.data)
+        signinHandler(res,  user, token)
+        res.render('/', { title: '' });
       }
-      res.render('login', { title: 'wrong input' });
+      res.render('/login', { title: 'wrong input' });
     } catch (error) {
       console.log(error);
       res.render('login', { title: 'error' });
   }
+
 });
 
+class Session {
+    constructor(username, expiresAt, hashedToken) {
+        this.username = username
+        this.expiresAt = expiresAt
+        this.token = hashedToken
+    }
+
+		// we'll use this method later to determine if the session has expired
+    isExpired() {
+        this.expiresAt < (new Date())
+    }
+}
+
+// this object stores the users sessions. For larger scale applications, you can use a database or cache for this purpose
+const sessions = {}
+
+const signinHandler = (res,username,token) => {
+
+    const sessionToken = token
+
+    // set the expiry time as 120s after the current time
+    const now = new Date()
+    const expiresAt = new Date(+now + 220 * 1000)
+
+    // create a session containing information about the user and expiry time
+    const session = new Session(username,token,expiresAt)
+    // add the session information to the sessions map
+    sessions[sessionToken] = session
+
+    console.log("cookie of session",sessions)
+    // In the response, set a cookie on the client with the name "session_cookie"
+    // and the value as the UUID we generated. We also set the expiry time
+    res.cookie("session_token", sessionToken, { expires: expiresAt })
+    res.end()
+}
 
 
 module.exports = router;
---
+++ variables.js
@@ -0,0 +1,7 @@
+const backendApi = "https://api.p4wsh.com/api";
+
+const globalConstant = {
+    backendApi
+}
+
+module.exports = globalConstant;
